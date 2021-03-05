db869f867f8e8ed0563a71ad6fe881020db9c260
Brandon Marceau <bran.13@hotmail.ca>
2021-03-05 17:51:32 -0500

    Paquet myapp #1
    Dossier avec tout les fichiers du paquet #1

---
+++ myapp/tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'myapp.exe'
+
+$packageArgs = @{
+  packageName   = 'myapp'
+  #unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  #url           = $url
+  #url64bit      = $url64
+  file 			= $fileLocation
+  softwareName  = 'myapp'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
